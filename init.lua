ff73bac91c9a510624aaaefb366fb506c30ed47a
Tobani E-G <tobanigeorge@gmail.com>
2025-07-10 18:01:57 +0100

    Whick key groups

--- init.lua
+++ init.lua
@@ -355,7 +355,8 @@ require('lazy').setup({
       spec = {
         { '<leader>s', group = '[S]earch' },
         { '<leader>t', group = '[T]oggle' },
-        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>g', group = '[G]it Hunk', mode = { 'n', 'v' } },
+        { '<leader>b', group = '[B]uffer', mode = { 'n', 'v' } },
       },
     },
   },
